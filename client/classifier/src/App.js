71aeaa4d5428848b5a9bc769895a9045b22ac645
jarturogl05 <jarturogl05@gmail.com>
2021-04-12 21:55:04 -0500

    layaout and styles

--- client/classifier/src/App.js
+++ client/classifier/src/App.js
@@ -1,8 +1,21 @@
+import React from 'react'
+import './App.css'
+import Dropzone from './components/Dropzone.js'
 
 function App() {
   return (
-    <div>
-        xd
+    <div className="app_container">
+        <div className="title_section">
+          <h1>Image Classifier</h1>
+        </div>
+
+        <div className = 'input_section'>
+            <h2>Select a image</h2>
+            <Dropzone></Dropzone>
+        </div>
+        <div className="results_section">
+            <h2>Results</h2>
+        </div>
     </div>
   );
 }
