221eed4b96522854fdfb5b4db41f701cb0acc896
jarturogl05 <jarturogl05@gmail.com>
2021-04-18 20:18:04 -0500

    uploadForm

--- client/classifier/src/components/FileUpload.js
+++ client/classifier/src/components/FileUpload.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './fileUpload.css'
 
 export default function FileUpload() {
+    const [selectedFile, setSelectedFile] = useState();
+    const [isFilePicked, setIsFilePicked] = useState(false);
+
+    function handleSumbit (e){
+        e.preventDefault();
+    }
+
+    function handleFile(e){
+        setSelectedFile(e.target.files[0]);
+        console.log(selectedFile);
+    }
+
     return (
-        <div>
-            
+        <div className="fileUpload_container">
+            <form onSubmit={handleSumbit}>
+                <input type="file" onChange={handleFile} accept="image/*"></input>
+                <button type="submit" className="fileUpload_sendButton">Send</button>
+            </form>
         </div>
     )
 }
